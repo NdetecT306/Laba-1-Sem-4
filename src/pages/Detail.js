02cc7845987bb577ac1077652ace065c8f409ecf
NdetecT306 <nstasya777@mail.ru>
2026-04-14 00:39:01 +0700

    Update Detail.js

--- src/pages/Detail.js
+++ src/pages/Detail.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect, useCallback } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
 import axios from 'axios';
 
-const API_URL = ''; // Временно пустая строка, позже замените на ваш Railway URL
+const API_URL = '/api';
 
 function Detail() {
   const { id } = useParams();
